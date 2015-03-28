14c1163260ad3a6a4db400a289dfeab4aebbfc74
ricarsant <jgarzon@hotmail.com>
2015-03-28 12:47:58 -0500

    js
    js

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -21,3 +21,20 @@ var ky;
 for(ky in my_object){
 	console.log(my_object[ky]);
 }
+
+
+// Funciones
+
+function test(){
+	console.log('ejecutando una funcion');
+}
+
+test();
+
+// usando funcion anonima
+
+var bye_bye = function(){
+	console.log('Que tengan una feliz semana santa');
+}
+
+bye_bye();
